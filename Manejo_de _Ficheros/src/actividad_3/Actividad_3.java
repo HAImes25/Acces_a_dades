733d4c35f3e70b3e843a25ff56d70049e7b50965
HAImes25 <IM2024032@imesmaresme.com>
2025-10-06 08:34:31 +0200

    Actividad 3 Paso 3

--- Manejo_de _Ficheros/src/actividad_3/Actividad_3.java
+++ Manejo_de _Ficheros/src/actividad_3/Actividad_3.java
@@ -1,8 +1,10 @@
 package actividad_3;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Actividad_3 {
     public static void main(String[] args) {
@@ -21,33 +23,33 @@ public class Actividad_3 {
                 System.out.println("El fichero ya existe.");
             }
 
+            // Write
             try(FileWriter writer = new FileWriter(fichero)){
                 for (int i = 0; i < 100; i++) {
                     writer.write((i+1) + "\n");
                 }
-
-
             }catch (IOException ex){
                 System.err.println("no se ha creado el fichero");
                 System.err.println(ex.getMessage());
             }
 
-
-
-
-
-
+            // Read
+            try (Scanner scanner = new Scanner(fichero.getAbsoluteFile())){
+                while (scanner.hasNext()){
+                    String linea = scanner.nextLine();
+                    int numero = Integer.parseInt(linea); // Pasar el String a Int
+                    if ((numero % 10) == 0){
+                        System.out.println(numero);
+                    }
+                }
+            }catch (FileNotFoundException e){
+                System.err.println(e.getMessage());
+            }
 
         }catch (IOException ex){
             System.err.println("no se ha creado el fichero");
             System.err.println(ex.getMessage());
         }
-
-
-
-
-
-
 
     }
 }
